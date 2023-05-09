ed58e021895bcd106225427d3e02e7aa0a537dac
Kisae Lee <88358722+KiSae-Lee@users.noreply.github.com>
2023-05-09 15:30:19 +0900

    Quick fix.

--- src/Components/Gemetry/GeometryRenderer.tsx
+++ src/Components/Gemetry/GeometryRenderer.tsx
@@ -2,22 +2,6 @@ import React from 'react';
 import PointRenderer from './Point';
 import LineRenderer from './Line';
 
-declare global {
-    const lines: LineProps[] = [];
-    const vertices = new Float32Array();
-
-    export interface PointProps {
-        x: number;
-        y: number;
-        z: number;
-    }
-
-    interface LineProps {
-        sPt: Vec3;
-        ePt: Vec3;
-    }
-}
-
 export const GeometryRenderer = () => {
     return (
         <>
